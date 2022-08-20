3c2ed97ec97cdbf5ae42211df9da7d044be07fa8
Bayu12345677 <bayuriski558@gmail.com>
2022-08-20 12:12:18 +0700

    Source file

---
+++ src/utils/generator-nitro/test.sh
@@ -0,0 +1,16 @@
+source lib/moduler.sh
+
+Namespace Std :: Sys;
+
+Bash.import: feature/io.echo
+Bash.import: minimist/args Shua/Ua
+
+#const: __say__ =\> say
+#minimist.argv ["$1"]
+#Named.argv "$1" ~\> [rp]
+const: __SHUA__ =\> Ua
+
+Ua.random
+#var p = "alokpaijah"
+#say.echo "$p"
+#io.write "halo bang"
